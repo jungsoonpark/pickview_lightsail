fba11719a0908887e16ee530870948cf7183c9ce
jungsoonpark <64699798+jungsoonpark@users.noreply.github.com>
2025-04-13 22:32:13 +0900

    Update buying_guide.py

--- buying_guide.py
+++ buying_guide.py
@@ -5,6 +5,7 @@ import logging
 import time
 from datetime import datetime
 import json
+import os
  
  # 로깅 설정
 logging.basicConfig(level=logging.INFO, format='[%(levelname)s] %(asctime)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
