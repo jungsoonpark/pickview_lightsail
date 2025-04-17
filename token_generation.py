0b172dce78055ff8c5eccaf6b9a0762acf593dd5
jungsoonpark <64699798+jungsoonpark@users.noreply.github.com>
2025-04-17 12:23:04 +0900

    Update token_generation.py

--- token_generation.py
+++ token_generation.py
@@ -7,7 +7,7 @@ import sys
 from github import Github
 
 # SDK 경로 추가
-sys.path.append('/home/runner/work/pickview_lightsail/pickview_lightsail/aliexpress_sdk/iop')
+path_to_iop = '/home/runner/work/pickview_lightsail/pickview_lightsail/aliexpress_sdk/iop'
 sys.path.append(path_to_iop)
 
 # 경로 추가 여부 확인
