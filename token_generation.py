c54e5228b33e19107c3371ca71ceb9f72c5fcd6f
jungsoonpark <64699798+jungsoonpark@users.noreply.github.com>
2025-04-17 16:34:28 +0900

    Update token_generation.py

--- token_generation.py
+++ token_generation.py
@@ -93,21 +93,24 @@ params["sign"] = generate_signature(params, secrets['api_secret'], "/rest/auth/t
 
 
 
-def get_github_secrets():
-    """GitHub Secrets에서 값을 가져옵니다."""
-    api_key = os.environ.get('API_KEY')  # GitHub Actions에서 설정한 API_KEY
-    api_secret = os.environ.get('API_SECRET')  # GitHub Actions에서 설정한 API_SECRET
+  def get_github_secrets():
+      """GitHub Secrets에서 값을 가져옵니다."""
+      api_key = os.environ.get('API_KEY')  # GitHub Actions에서 설정한 API_KEY
+      api_secret = os.environ.get('API_SECRET')  # GitHub Actions에서 설정한 API_SECRET
 
-    logger.debug(f"API Key: {api_key}")
-    logger.debug(f"API Secret: {api_secret}")
+      logger.debug(f"API Key: {api_key}")
+      logger.debug(f"API Secret: {api_secret}")
 
-    if api_key is None or api_secret is None:
-        logger.error("API Key or Secret is missing in GitHub Secrets!")
+      if api_key is None or api_secret is None:
+          logger.error("API Key or Secret is missing in GitHub Secrets!")
 
-    return {
-        "api_key": api_key,
-        "api_secret": api_secret
-    }
+      return {
+          "api_key": api_key,
+          "api_secret": api_secret
+      }
+
+
+
 
 
 def request_access_token(secrets, authorization_code):
