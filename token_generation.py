c32195326ce3f491c9aeee855453ce67af155573
jungsoonpark <64699798+jungsoonpark@users.noreply.github.com>
2025-04-17 16:12:05 +0900

    Update token_generation.py

--- token_generation.py
+++ token_generation.py
@@ -90,7 +90,9 @@ def get_github_secrets():
     api_key = os.environ.get('API_KEY')  # GitHub Actions에서 설정한 API_KEY
     api_secret = os.environ.get('API_SECRET')  # GitHub Actions에서 설정한 API_SECRET
 
-    # 값이 None이라면 오류를 출력하도록 합니다.
+    logger.debug(f"API Key: {api_key}")
+    logger.debug(f"API Secret: {api_secret}")
+
     if api_key is None or api_secret is None:
         logger.error("API Key or Secret is missing in GitHub Secrets!")
 
