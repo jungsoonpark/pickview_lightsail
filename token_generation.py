d9da177b21f13369f97b23bbd2f9253d3e628b0c
jungsoonpark <64699798+jungsoonpark@users.noreply.github.com>
2025-04-18 19:06:55 +0900

    Update token_generation.py

--- token_generation.py
+++ token_generation.py
@@ -48,7 +48,7 @@ def request_access_token():
     params = {
         "app_key": app_key,  # 실제 app_key
         "timestamp": str(int(time.time() * 1000)),  # 밀리초로 타임스탬프
-        "sign_method": "hmac",  # 서명 방법 md5
+        "sign_method": "md5",  # 서명 방법 md5
         "code": authorization_code,  # authorization_code
         "grant_type": "authorization_code",  # grant_type
     }
